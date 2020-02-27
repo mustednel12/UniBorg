b2cb14afb754c6baeca863af6176fd9da348d3d8
mustednel12 <58996426+mustednel12@users.noreply.github.com>
2020-02-27 23:29:49 +0300

    Update GenerateStringSession.py

--- GenerateStringSession.py
+++ GenerateStringSession.py
@@ -11,8 +11,8 @@ print("""Please go-to my.telegram.org
 Login using your Telegram account
 Click on API Development Tools
 Create a new application, by entering the required details""")
-APP_ID = int(input("806023"))
-API_HASH = input("5445492ff228c482bdbe234cbb99f66f")
+APP_ID = int(input(""))
+API_HASH = input("")
 
 with TelegramClient(StringSession(), APP_ID, API_HASH) as client:
     print(client.session.save())
